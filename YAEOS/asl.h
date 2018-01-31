eb95f5bd7cbb2bf2c75dfce81ac89e151add179f
GFdevelop <gabriele@archlinux.localdomain>
2018-01-31 16:05:37 +0100

    merged master

--- YAEOS/asl.h
+++ YAEOS/asl.h
@@ -24,7 +24,7 @@ pcb_t *headBlocked(int *key);
 
 pcb_t* removeBlocked(int *key);
 
-void forallBlocked(int *key, void fun(pcb_t *pcb, void *), void *arg);
+void forallBlocked(int *key, void (*fun)(pcb_t *pcb, void *), void *arg);
 
 //outChildBlocked(pcb_t *p);
 
