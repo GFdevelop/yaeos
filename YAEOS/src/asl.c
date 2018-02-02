51074f628d4cd32e097d7a2adb093d00a062ace8
GFdevelop <gabriele@archlinux.localdomain>
2018-02-02 18:02:31 +0100

    last error line is 329, elements are 2 instead of 3 why?!?!

--- YAEOS/src/asl.c
+++ YAEOS/src/asl.c
@@ -10,30 +10,41 @@ semd_t *semdhash[ASHDSIZE];
 int insertBlocked(int *key, pcb_t *p){
 	long k = (*key)*61803398875;
 	int hash = (8*(k-(int)k))-(10^10);
-	if (semdhash[hash] == NULL) {
+	if (semdhash[hash] == NULL){
 		if (semdFree_h == &semd_table[MAXSEMD]) return -1;
 		else {
 			semdhash[hash] = semdFree_h;
-			semdFree_h->s_next = NULL;
+			semdhash[hash]->s_next = NULL;
 			semdFree_h = semdFree_h+1;
-			return 0;
 		}
 	}
+	insertProcQ(&(*semdhash)[hash].s_procQ,p);
+	return 0;
+	
+	/*if (semdFree_h == &semd_table[MAXSEMD]) return -1;
 	else {
-		if (semdFree_h == &semd_table[MAXSEMD]) return -1;
+		if (semdhash[hash] == NULL){
+			semdhash[hash] = semdFree_h;
+			semdFree_h->s_next = NULL;
+		}
 		else {
 			semd_t * tmp = semdhash[hash];
 			semdhash[hash] = semdFree_h;
 			semdFree_h->s_next = tmp;
-			semdFree_h = semdFree_h+1;
-			return 0;
 		}
-	}
+		semdFree_h = semdFree_h+1;
+		insertProcQ(&(*semdhash)[hash].s_procQ,p);
+		return 0;
+	}*/
 }
 
 pcb_t *headBlocked(int *key){
-	pcb_t * temp;
-	return temp;
+	long k = (*key)*61803398875;
+	int hash = (8*(k-(int)k))-(10^10);
+	if (semdhash[hash] == NULL) return NULL;
+	else {
+		headProcQ((*semdhash)[hash].s_procQ);
+	}
 }
 
 pcb_t* removeBlocked(int *key){
@@ -42,7 +53,9 @@ pcb_t* removeBlocked(int *key){
 }
 
 void forallBlocked(int *key, void (*fun)(pcb_t *pcb, void *), void *arg){
-	
+	long k = (*key)*61803398875;
+	int hash = (8*(k-(int)k))-(10^10);
+	if (semdhash[hash] != NULL) forallProcQ((*semdhash)[hash].s_procQ, fun, arg);
 }
 
 //outChildBlocked(pcb_t *p);
