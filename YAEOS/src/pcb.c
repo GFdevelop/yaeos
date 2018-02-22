e98e7a4eeaf05e5711383d4642aa2fea01eaa80a
Mattia Polverini <mattia.polverini@studio.unibo.it>
2018-02-22 02:56:28 +0100

    Comments on pcb.c completed

--- YAEOS/src/pcb.c
+++ YAEOS/src/pcb.c
@@ -15,23 +15,24 @@
 #include "asl.h"
 
 pcb_t pcbFree_table[MAXPROC];
+/*
+Assign to the pointer pcbfree_h the memory address of the next memory location after the last element of pcbFree_table.
+See point [1] of design_choises.txt.
+*/
 pcb_t *pcbfree_h = &pcbFree_table[MAXPROC];
 
 void initPcbs(){
-	int size = pcbfree_h-pcbFree_table-1;	//differenza fra due indirizzi di memoria scelta progettuale 1
-	if (size < 0) return;
-	else {
-		pcbfree_h = pcbfree_h-1;	//index of array
+	int size = pcbfree_h - pcbFree_table;	//From the difference between the two pointer we obtain the actual number of elements.
+	if (pcbfree_h > pcbFree_table){
+		pcbfree_h = pcbfree_h - 1;	//The pointer is used as index for the array.
 		initPcbs();
-		if (size < MAXPROC - 1)	(pcbFree_table[size]).p_next = &pcbFree_table[size+1];
-		else pcbFree_table[size].p_next = NULL;
+		pcbFree_table[size-1].p_next = (size < MAXPROC) ? &pcbFree_table[size] : NULL;
 	}
 }
 
 void freePcb(pcb_t *p){
 	if (p != NULL){
-		int size = pcbfree_h-pcbFree_table-1;
-		if (size < MAXPROC){
+		if ((pcbfree_h - pcbFree_table - 1) < MAXPROC){	//Included to avoid not allowed insertion of pcb_t
 			p->p_next = pcbfree_h;
 			pcbfree_h = p;
 		}
@@ -52,24 +53,21 @@ pcb_t *allocPcb(){
 }
 
 void insertProcQ(pcb_t **head, pcb_t *p){
-	if (p != NULL){
-		if (*head == NULL){
+	if (p != NULL){	//The insertion can be divided into three cases...
+		if (*head == NULL){	//...list pointed by head is empty 
 			*head = p;
 			(*head)->p_next = NULL;
-		}
-		else if (p->p_priority > (*head)->p_priority){
+		}else if (p->p_priority > (*head)->p_priority){	//...priority of element p > priority of the current one
 			p->p_next = *head;
 			*head = p;
+		}else{	//...priority of the current element > priority of the element p
+			insertProcQ(&(*head)->p_next, p);	
+			if ((*head)->p_next == p->p_next) (*head)->p_next = p;
 		}
-		else if ((*head)->p_next == NULL){
-			(*head)->p_next = p;
-			p->p_next = NULL;
-		}
-		else insertProcQ(&(*head)->p_next, p);
 	}
 }
 
-pcb_t *headProcQ(pcb_t *head){
+pcb_t *headProcQ(pcb_t *head){ //NULL (empty list) case not needed: if list is empty head == NULL
 	return head;
 }
 
@@ -82,10 +80,13 @@ pcb_t* removeProcQ(pcb_t **head){
 	}
 }
 
-pcb_t* outProcQ(pcb_t **head, pcb_t *p){	//vai a scelte progettuali 2
-	if ((p == NULL) || (*head == NULL)) return NULL;
-	else if (*head == p) return removeProcQ(&(*head));
-	else return outProcQ(&(*head)->p_next, p);
+/*
+For more information, see point [2] in design_choices.txt
+*/
+pcb_t* outProcQ(pcb_t **head, pcb_t *p){	//Four possible scenarios...
+	if ((p == NULL) || (*head == NULL)) return NULL;	//...p is NULL or list is empty/p is not found
+	else if (*head == p) return removeProcQ(&(*head)); //...p is the element pointed by head
+	else return outProcQ(&(*head)->p_next, p);	//...p not found but list isn't finished yet
 }
 
 void forallProcQ(pcb_t *head, void fun(pcb_t *pcb, void *), void *arg){
@@ -95,15 +96,17 @@ void forallProcQ(pcb_t *head, void fun(pcb_t *pcb, void *), void *arg){
 	}
 }
 
+/*
+See desing_choices.txt point [3]
+*/
 void insertChild(pcb_t *parent, pcb_t *p){
 	if ((parent != NULL) && (p != NULL)){
-		if (parent->p_first_child == NULL) {
+		if (parent->p_first_child == NULL){	//parent has no child
 			p->p_sib = NULL;
 			parent->p_first_child = p;
 			p->p_parent = parent;
-		}
-		else {
-			pcb_t * son = parent->p_first_child;
+		}else{	//parent has already one or more children
+			pcb_t *son = parent->p_first_child;
 			parent->p_first_child = son->p_sib;
 			insertChild(parent,p);
 			parent->p_first_child = son;
@@ -114,22 +117,19 @@ void insertChild(pcb_t *parent, pcb_t *p){
 
 pcb_t *removeChild(pcb_t *p){
 	if ((p == NULL) || (p->p_first_child == NULL)) return NULL;
-	else  {
+	else{
 		pcb_t * ret = p->p_first_child;
 		p->p_first_child = ret->p_sib;
-		ret->p_sib = NULL;
-		ret->p_parent = NULL;
 		return ret;
 	}
 }
 
+/*
+Complete information at point [4] in design_choices.txt
+*/
 pcb_t *outChild(pcb_t *p){
 	if ((p == NULL) || (p->p_parent == NULL) || (p->p_parent->p_first_child == NULL)) return NULL;
-	else if (p == p->p_parent->p_first_child){
-		p->p_parent->p_first_child = p->p_sib;
-		p->p_sib = NULL;
-		return p;
-	}
+	else if (p == p->p_parent->p_first_child) return removeChild(p->p_parent);
 	else {
 		pcb_t * son = p->p_parent->p_first_child;
 		p->p_parent->p_first_child = son->p_sib;
@@ -138,4 +138,4 @@ pcb_t *outChild(pcb_t *p){
 		p->p_parent->p_first_child = son;
 		return ret;
 	}
-}
+} 
