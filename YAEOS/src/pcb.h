c3ed75f897df8375e74c66b84d62bfe85b6078ba
Mattia Polverini <mattia.polverini@studio.unibo.it>
2018-02-22 01:45:16 +0100

    *.h files' comments completed.

--- YAEOS/src/pcb.h
+++ YAEOS/src/pcb.h
@@ -12,57 +12,76 @@ typedef struct pcb_t {
 	state_t p_s;
 	int p_priority;
 	int *p_semKey;	
-} pcb_t;
+}pcb_t;
 
 /**** PCB queue management ****/
 
 /* 
-Initializes the pcbFree list in order to contain all the elements of the
-pcbFree_table. This method will be invoked only once in the 
-initialization phase of the data structure
+Initializes the pcbFree list in order to contain all the elements of the pcbFree_table. 
+This method will be invoked only once in the initialization phase of the data structure.
 */
 void initPcbs();
 
-/* Insert the PCB pointed by *p in the free PCB list */
+/* 
+Insert the PCB pointed by p in the pcbFree list. 
+*/
 void freePcb(pcb_t *p);
 
-/* Remove a element from free PCB list, initialize it and return it.
-   Return NULL if the free PCB list is empty. */
+/*
+Remove an element from pcbFree list, initialize and return it.
+Return NULL if the pcbFree list is empty. 
+*/
 pcb_t *allocPcb();
 
-/* Insert the element pointed by *p in the process queue pointed by **head.
-   Insertion have to respect the priority of each PCB.
-   Process queue must be descending sorted according to PCB's priority. */
+/* 
+Insert the element pointed by p in the process queue pointed by head.
+Insertion must respect the priority of each pcb.
+Process queue must be descending sorted according to pcb's priority. 
+*/
 void insertProcQ(pcb_t **head, pcb_t *p);
 
-/* Return the element in the head of queue pointed by pointer passed as pointer, without removing it.
-   Return NULL if the queue is empty. */
+/* 
+Return the first element of the queue pointed by head, without removing it.
+Return NULL if the queue is empty. 
+*/
 pcb_t *headProcQ(pcb_t *head);
 
-/* Remove the first element in the queue pointed by the argument.
-   Return a poiter to the removed element or NULL if the queue is empty. */
+/* 
+Remove the first element in the queue pointed by head.
+Return a poiter to the removed element or NULL if the queue is empty. 
+*/
 pcb_t* removeProcQ(pcb_t **head);
 
-/* Remove the element pointed by *p from the queue pointed by the first argument.
-   Return a pointer to p if it is present, NULL otherwise. */
+/* 
+Remove the element pointed by p from the queue pointed by head.
+Return a pointer to p if it is present, NULL otherwise. 
+*/
 pcb_t* outProcQ(pcb_t **head, pcb_t *p);
 
-/* For each element in the queue pointed by head, call the function fun(...) */
+/*
+For each element in the queue pointed by head, call the function fun(...).
+*/
 void forallProcQ(pcb_t *head, void fun(pcb_t *pcb, void *), void *arg);
 
 
 /**** PCB tree management ****/
 
-/* Insert the PCB pointed by p as a child of PCB pointed by parent */
+/*
+Insert the pcb pointed by p as child of the pcb pointed by parent. 
+*/
 void insertChild(pcb_t *parent, pcb_t *p);
 
-/* Remove the first child of the PCB pointed by p and return its pointer. 
-   Return NULL if p has no child */
+/*
+Remove the first child of the pcb pointed by p and return its pointer. 
+Return NULL if p has no child 
+*/
 pcb_t *removeChild(pcb_t *p);
 
-/* Remove PCB pointed by p from the child list of his father.
-   If PCB has no father, it returns NULL.
-   Return a pointer to the removed element otherwise. */
+/*
+Remove the pcb pointed by p from the child list of his father.
+If p has no father, it returns NULL.
+Return a pointer to the removed element otherwise. 
+*/
 pcb_t *outChild(pcb_t *p);
 
 #endif
