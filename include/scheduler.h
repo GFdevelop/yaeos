76baadd318c674f1e1269279a22c555f626c6ff6
GFdevelop <17930457+GFdevelop@users.noreply.github.com>
2018-09-21 17:23:55 +0200

    slice not work

--- include/scheduler.h
+++ include/scheduler.h
@@ -1,9 +1,9 @@
 #ifndef SCHEDULER_H
 #define SCHEDULER_H
 
-#define SLICE_TIME 3000
-#define AGING_TIME 10000
-#define TICK_TIME 100000
+#define SLICE_TIME 3000UL
+#define AGING_TIME 10000UL
+#define TICK_TIME 100000UL
 
 void scheduler();
 
